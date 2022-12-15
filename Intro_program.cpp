840cf9b785f7a3413fbcd5a57d7ee2f625e90ed6
MD KAUSAR HAMID MIJI <107298603+git-miji@users.noreply.github.com>
2022-12-15 00:04:08 -0700

    Add files via upload

---
+++ Intro_program.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main()
+
+{
+    cout << "Hi!, Kausar Hamid Miji!" << " Welcome to the world of C++ programming!!!";
+    getch();
+}
