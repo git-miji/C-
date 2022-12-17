4f7f27753b1a946e0a2a224defa459b5ab60c826
MD KAUSAR HAMID MIJI <107298603+git-miji@users.noreply.github.com>
2022-12-16 17:03:17 -0700

    Add files via upload

---
+++ String_Variable.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<conio.h>
+
+using namespace std;
+
+int main()
+{
+    /*
+    To declare string as variable, we need to mention the length
+    of the character.
+    */
+
+    char name[21] = "Md Kausar Hamid Miji";
+    cout << "My Name is: " << name;
+    getch();
+}
