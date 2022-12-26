fb45ba2a5713472b26041b9d5f2047de630c3537
MD KAUSAR HAMID MIJI <107298603+git-miji@users.noreply.github.com>
2022-12-26 00:14:40 -0700

    Add files via upload

---
+++ 18_Grade_Calculation.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main()
+{
+    int mark;
+    cout << endl;
+    cout << "Enter the mark: " << endl;
+    cin >> mark;
+    cout << endl;
+
+    if (mark > 100)
+    {
+        cout << "Your entered mark is invalid!" << endl;
+    }
+    else if (mark >= 90 && mark <=100)
+    {
+        cout << "You received grade A for this course!" << endl;
+    }
+    else if (mark >= 80 && mark <90)
+    {
+        cout << "You received grade B for this course!" << endl;
+    }
+    else if (mark >= 70 && mark <80)
+    {
+        cout << "You received grade C for this course!" << endl;
+    }
+    else if (mark >= 60 && mark <70)
+    {
+        cout << "You received grade D for this course!" << endl;
+    }
+    else
+    {
+        cout << "You have failed in this course :(" << endl;
+    }
+
+
+
+
+
+
+
+    getch();
+}
