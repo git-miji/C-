e5f36671e83e5e954dab382189eee87446d15048
MD KAUSAR HAMID MIJI <107298603+git-miji@users.noreply.github.com>
2023-01-03 21:35:31 -0700

    Updated educational Information

--- 01_Intro_program.cpp
+++ 01_Intro_program.cpp
@@ -5,6 +5,9 @@ using namespace std;
 int main()
 
 {
-    cout << "Hi!, Kausar Hamid Miji!" << " Welcome to the world of C++ programming!!!";
+    cout << "Hi!, Kausar Hamid Miji!" << " Welcome to the world of C++ programming!!!" << endl;
+    cout << "Learning C++ is fun!!!" << endl;
+    cout << "C++ is an object oriented language." << endl;
+    cout << "I am studying Computer Science at SD Mines!" << endl;
     getch();
 }
