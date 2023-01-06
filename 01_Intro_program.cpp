b5b36c825c0eaaa735ae97211b8948dc4ca8aa7f
git-miji <write2miji@gmail.com>
2023-01-06 00:34:03 -0700

    Updated prorgams

--- 01_Intro_program.cpp
+++ 01_Intro_program.cpp
@@ -1,18 +1,3 @@
-<<<<<<< HEAD
-#include<iostream>
-#include<conio.h>
-using namespace std;
-
-int main()
-
-{
-    cout << "Hi!, Kausar Hamid Miji!" << " Welcome to the world of C++ programming!!!" << endl;
-    cout << "Learning C++ is fun!!!" << endl;
-    cout << "C++ is an object oriented language." << endl;
-    cout << "I am studying CS at SD Mines!" << endl;
-    getch();
-}
-=======
 #include<iostream>
 #include<conio.h>
 using namespace std;
@@ -26,4 +11,3 @@ int main()
     cout << "I am studying Computer Science at SD Mines!" << endl;
     getch();
 }
->>>>>>> 3e506be22e2522730be1330f87e17641d92262a3
