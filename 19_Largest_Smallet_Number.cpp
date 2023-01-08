872c4fc4c7bcf86e09faef64b0f8b0c5ec094957
git-miji <write2miji@gmail.com>
2023-01-07 21:04:56 -0700

    Updated program

--- 19_Largest_Smallet_Number.cpp
+++ 19_Largest_Smallet_Number.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include<iostream>
 #include<conio.h>
 using namespace std;
@@ -46,52 +45,3 @@ int main()
     }
     getch();
 }
-=======
-#include<iostream>
-#include<conio.h>
-using namespace std;
-
-int main()
-{
-    //Finding the largest number
-
-    int p, q, r;
-    cout << "Enter 3 Numbers: " <<endl;
-    cin >> p >> q >> r;
-    cout << endl;
-
-    if (p > q && q > r)
-    {
-        cout << p << " is the largest number! " <<endl;
-    }
-    else if (q > p && q > r)
-    {
-        cout << q << " is the largest number! " <<endl;
-    }
-    else
-    {
-        cout << r << " is the largest number! " <<endl;
-    }
-
-    //Finding the Smallest number
-
-    int a, b, c;
-    cout << "Enter 3 Numbers: " <<endl;
-    cin >> a >> b >> c;
-    cout << endl;
-
-    if (a < b && b < c)
-    {
-        cout << a << " is the smallest number! " <<endl;
-    }
-    else if (b < a && a < c)
-    {
-        cout << b << " is the smallest number! " <<endl;
-    }
-    else
-    {
-        cout << c << " is the smallest number! " <<endl;
-    }
-    getch();
-}
->>>>>>> 3e506be22e2522730be1330f87e17641d92262a3
--- 20_Leap_Year.cpp
+++ 20_Leap_Year.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include<iostream>
 #include<conio.h>
 using namespace std;
@@ -23,29 +22,3 @@ int main()
     }
     getch();
 }
-=======
-#include<iostream>
-#include<conio.h>
-using namespace std;
-
-int main()
-{
-    //Finding the leap year
-    // If a year is divisible by 4 then it's a leap year.
-
-    int year;
-    cout << "Enter Your Desired Year: " <<endl;
-    cin >> year;
-    cout << endl;
-
-    if (year % 4 == 0)
-    {
-        cout << year << " is Leap Year!!!";
-    }
-    else
-    {
-        cout << year << " is not Leap year! " <<endl;
-    }
-    getch();
-}
->>>>>>> 3e506be22e2522730be1330f87e17641d92262a3
