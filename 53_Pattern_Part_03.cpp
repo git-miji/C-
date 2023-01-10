5b80c9f5335d0bb7bdc10cdc1e71238953b7cd91
git-miji <write2miji@gmail.com>
2023-01-09 22:59:13 -0700

    Updated program to print pattern using nested loop

--- 53_Pattern_Part_03.cpp
+++ 53_Pattern_Part_03.cpp
@@ -39,5 +39,21 @@ int main()
 
         cout << endl;
     }
+
+    cout << "Printing Hash Symbol pattern" << endl;
+    cout << "****************************" << endl;
+
+    cout << "Enter the number of lines: ";
+    cin >> n;
+    cout << endl;
+    for (row=1; row<=n; row++)
+    {
+        for (column=1; column<=row; column++)
+        {
+            cout << " " << " # ";
+        }
+
+        cout << endl;
+    }
     getch();
 }
