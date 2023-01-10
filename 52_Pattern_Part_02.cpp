985c2512eb8c6ba44e82d93397b38d4c088fb748
git-miji <write2miji@gmail.com>
2023-01-09 22:54:26 -0700

    Updated program to print pattern using nested loop

--- 52_Pattern_Part_02.cpp
+++ 52_Pattern_Part_02.cpp
@@ -7,8 +7,8 @@ int main()
 {
     int n, row, column;
 
-    cout << "Printing Character pattern" << endl;
-    cout << "**************************" << endl;
+    cout << "Printing Capital Letter pattern" << endl;
+    cout << "*******************************" << endl;
 
     cout << "Enter the number of lines: ";
     cin >> n;
@@ -19,6 +19,49 @@ int main()
         for (column=1; column<=row; column++)
         {
             cout << " " << char(column+64);
+        }
+
+        cout << endl;
+    }
+
+    cout << "Enter the number of lines: ";
+    cin >> n;
+    cout << endl;
+    for (row=1; row<=n; row++)
+    {
+        for (column=1; column<=row; column++)
+        {
+            cout << " " << char(row+64);
+        }
+
+        cout << endl;
+    }
+
+    cout << "Printing Small Letter pattern" << endl;
+    cout << "*****************************" << endl;
+
+    cout << "Enter the number of lines: ";
+    cin >> n;
+    cout << endl;
+
+    for (row=1; row<=n; row++)
+    {
+        for (column=1; column<=row; column++)
+        {
+            cout << " " << char(column+96);
+        }
+
+        cout << endl;
+    }
+
+    cout << "Enter the number of lines: ";
+    cin >> n;
+    cout << endl;
+    for (row=1; row<=n; row++)
+    {
+        for (column=1; column<=row; column++)
+        {
+            cout << " " << char(row+96);
         }
 
         cout << endl;
