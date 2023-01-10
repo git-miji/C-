6e81ba4f1a3702cfe65b571b1afe012a847397c9
git-miji <write2miji@gmail.com>
2023-01-09 22:32:21 -0700

    Updated program to print pattern using nested loop

--- 51_Pattern_Part_01.cpp
+++ 51_Pattern_Part_01.cpp
@@ -20,5 +20,108 @@ int main()
 
         cout << endl;
     }
+
+    cout << "Enter the number of lines: ";
+    cin >> n;
+    cout << endl;
+
+    for (row=1; row<=n; row++)
+    {
+        for (column=1; column<=row; column++)
+        {
+            cout << " " << row;
+        }
+
+        cout << endl;
+    }
+
+
+    cout << "Enter the number of lines: ";
+    cin >> n;
+    cout << endl;
+
+    for (row=1; row<=n; row++)
+    {
+        for (column=1; column<=row; column++)
+        {
+            cout << " " << row % 2;
+        }
+
+        cout << endl;
+    }
+
+
+    cout << "Enter the number of lines: ";
+    cin >> n;
+    cout << endl;
+
+    for (row=1; row<=n; row++)
+    {
+        for (column=1; column<=row; column++)
+        {
+            cout << " " << row / 2;
+        }
+
+        cout << endl;
+    }
+
+
+    cout << "Enter the number of lines: ";
+    cin >> n;
+    cout << endl;
+
+    for (row=1; row<=n; row++)
+    {
+        for (column=1; column<=row; column++)
+        {
+            cout << " " << row % 2 <<" " << row / 2;
+        }
+
+        cout << endl;
+    }
+
+    cout << "Enter the number of lines: ";
+    cin >> n;
+    cout << endl;
+
+    for (row=1; row<=n; row++)
+    {
+        for (column=1; column<=row; column++)
+        {
+            cout << " " << column % 2;
+        }
+
+        cout << endl;
+    }
+
+
+    cout << "Enter the number of lines: ";
+    cin >> n;
+    cout << endl;
+
+    for (row=1; row<=n; row++)
+    {
+        for (column=1; column<=row; column++)
+        {
+            cout << " " << column / 2;
+        }
+
+        cout << endl;
+    }
+
+    cout << "Enter the number of lines: ";
+    cin >> n;
+    cout << endl;
+
+    for (row=1; row<=n; row++)
+    {
+        for (column=1; column<=row; column++)
+        {
+            cout << " " << column % 2 <<" " << column / 2;
+        }
+
+        cout << endl;
+    }
+
     getch();
 }
