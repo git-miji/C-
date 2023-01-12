6ddae4f6fe5883967c61a3b31c9f3d601e8d536b
git-miji <write2miji@gmail.com>
2023-01-11 23:54:14 -0700

    Added a program to print pattern using nested loop!

---
+++ 59_Pattern_Part_09.cpp
@@ -0,0 +1,55 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main()
+{
+    int n, row, column;
+    cout <<"Enter the number of Row: ";
+    cin >> n;
+    cout << endl;
+
+    cout <<"Printing Number Pattern" << endl;
+    cout <<"***********************" << endl;
+
+    for (row=n; row>0; row--)
+    {
+        //Printing Space
+        for (column=1; column<=n-row; column++)
+        {
+            cout <<" ";
+        }
+        // Printing Number
+        for (column=1; column<=row; column++)
+        {
+            cout <<column;
+        }
+        cout <<endl;
+
+    }
+
+    cout << endl;
+    cout <<"Printing Number Pattern" << endl;
+    cout <<"***********************" << endl;
+
+    for (row=n; row>0; row--)
+    {
+        //Printing Space
+        for (column=1; column<=n-row; column++)
+        {
+            cout <<" ";
+        }
+        // Printing Number
+        for (column=1; column<=row; column++)
+        {
+            cout <<column;
+        }
+        cout <<endl;
+
+    }
+
+
+
+    getch();
+}
+
