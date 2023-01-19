f763f1ee21959e6295c6c9743b6819191f135fa8
git-miji <write2miji@gmail.com>
2023-01-19 00:35:18 -0700

    Added Triangle Star Pattern Printing program!

---
+++ 68_X_Star_Pattern.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main()
+{
+    int n, row, column;
+    cout <<"Enter the number of Row: ";
+    cin >> n;
+    cout << endl;
+
+    cout <<"Printing X Star Pattern" << endl;
+    cout <<"***********************" << endl;
+
+    for (row=1; row<=n; row++)
+    {
+        //Printing Star
+        for (column=1; column<=n; column++)
+        {
+            if (row+column==n+1 || row==column)
+            {
+                cout <<" * ";
+            }
+        // Printing Space
+            else
+            {
+                cout <<"   ";
+            }
+        }
+        cout << endl;
+    }
+    getch();
+}
