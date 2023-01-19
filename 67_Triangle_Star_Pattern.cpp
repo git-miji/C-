58ef43210307bf430d4a363e02148deee3f7c9ea
git-miji <write2miji@gmail.com>
2023-01-19 00:27:14 -0700

    Added Triangle Star Pattern Printing program!

---
+++ 67_Triangle_Star_Pattern.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main()
+{
+    int n, row, column;
+    cout <<"Enter the number of Row: ";
+    cin >> n;
+    cout << endl;
+
+    cout <<"Printing Rectangle Star Pattern" << endl;
+    cout <<"*******************************" << endl;
+
+    for (row=1; row<=n; row++)
+    {
+        //Printing Star
+        for (column=1; column<=n; column++)
+        {
+            if (row==n || column==1 || row==column)
+            {
+                cout <<" * ";
+            }
+        // Printing Space
+            else
+            {
+                cout <<"   ";
+            }
+        }
+        cout << endl;
+    }
+    getch();
+}
